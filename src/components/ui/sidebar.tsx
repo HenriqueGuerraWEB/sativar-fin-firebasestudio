f2751c210c2c671c15b3047f4c249156467a30f4
Henrique Guerra <sunrise.henrique@gmail.com>
2025-08-06 03:49:34 +0000

    na sidebar o item de menu no hover e no item ativo, vamos usar bg rounde

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -45,7 +45,7 @@ const SidebarProvider = React.forwardRef<
 >(
   (
     {
-      defaultOpen = true,
+      defaultOpen = false,
       className,
       style,
       children,
@@ -83,7 +83,7 @@ const SidebarProvider = React.forwardRef<
               className
             )}
             ref={ref}
-            data-state={state}
+            data-state="collapsed"
             {...props}
           >
             {children}
@@ -252,14 +252,14 @@ const SidebarMenuItem = React.forwardRef<
 SidebarMenuItem.displayName = "SidebarMenuItem"
 
 const sidebarMenuButtonVariants = cva(
-  "peer/menu-button flex w-full items-center gap-3 overflow-hidden rounded-md p-2 text-left text-sm font-medium outline-none ring-sidebar-ring transition-all hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[active=true]:bg-sidebar-primary data-[active=true]:text-sidebar-primary-foreground justify-center !p-2 [&>svg]:size-5 [&>svg]:shrink-0",
+  "peer/menu-button flex items-center gap-3 overflow-hidden rounded-full p-2 text-left text-sm font-medium outline-none ring-sidebar-ring transition-all hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[active=true]:bg-sidebar-primary data-[active=true]:text-sidebar-primary-foreground justify-center !p-2 [&>svg]:size-5 [&>svg]:shrink-0",
   {
     variants: {
       variant: {
         default: "",
       },
       size: {
-        default: "h-10 text-sm",
+        default: "h-10 w-10 text-sm",
       },
     },
     defaultVariants: {
