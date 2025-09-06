6e95c19cbff7ede84a9bd235edcffd662b7fc926
Henrique Guerra <sunrise.henrique@gmail.com>
2025-09-06 18:02:30 +0000

    não estou entendendo o problema, recentemente mudei o plano de blaze par

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   projectId: "sativar-firebase",
   appId: "1:741613626340:web:3d17f6cad0c3d187641df1",
   storageBucket: "sativar-firebase.firebasestorage.app",
-  apiKey: "AIzaSyAHAX7P-E2jZlOUZMC1AO8AuAAItiXDrOI",
+  apiKey: "AlzaSyAHAX7P-E2jZIOUZMC1A08AuAAltiXDrOl",
   authDomain: "sativar-firebase.firebaseapp.com",
   measurementId: "",
   messagingSenderId: "741613626340",
