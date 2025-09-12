e706bc31020b784287661c303516a4caa9c474ca
Henrique Guerra <sunrise.henrique@gmail.com>
2025-09-12 13:01:35 +0000

    Precisamos fazer uma analise profunda no projeto e verificar as lógicas

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -9,11 +10,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
   projectId: "sativar-firebase",
   appId: "1:741613626340:web:3d17f6cad0c3d187641df1",
-  storageBucket: "sativar-firebase.firebasestorage.app",
-  apiKey: "AlzaSyAHAX7P-E2jZIOUZMC1A08AuAAltiXDrOl",
+  storageBucket: "sativar-firebase.appspot.com",
+  apiKey: "AIzaSyAHAX7P-E2jZIOUZMC1A08AuAAltiXDrOI",
   authDomain: "sativar-firebase.firebaseapp.com",
   messagingSenderId: "741613626340",
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
